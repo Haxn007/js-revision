4f933a00d90f70ad685fb0e6f759c2eb4ad535ae
Hassan <haxn007@gmail.com>
2023-09-28 18:02:03 +0000

    Learned variables

---
+++ basics-js/variables.js
@@ -0,0 +1,14 @@
+const accountId = 223333;
+let accountEmail = 'hh@gmail.com'
+var accountPassword = "11223344"
+accountCity = 'Lahore'
+let accountState;
+
+
+accountEmail = 'hh@g.com'
+accountPassword = '4455'
+accountCity = 'karachi'
+
+// accountId =33
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
